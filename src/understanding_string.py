980ef245c99fe28fa8fd24124a48a3ff6784f4bd
Tu Nombre <tu@email.com>
2025-11-03 10:39:05 -0600

    Avanze de programacion

--- src/understanding_string.py
+++ src/understanding_string.py
@@ -104,5 +104,54 @@ print(message)
 f_string_message = f"{famous_person_upv} {quote_of_famous_person}"
 print(f_string_message)
 
-rata = "Eliezer es marica"
-print(rata)
+# Numbers 
+"""
+Enteros
+
+Los podemos sumas (+), restar (-), multiplicar (*), divididir (/).
+Les podemos aplicar potencias(**2, **3, **3), modulo (%)
+"""
+
+number_1 = 35
+number_2 = 35
+suma = number_1+number_2
+resta = number_1-number_2
+multiplicacion = number_1*number_2
+division = number_1/number_2
+potencia = number_1**2
+modulo = number_1%number_2
+print("Suma: ", suma, type(suma))
+print("Resta: ", resta, type(resta))
+print("Multiplicacion: ", multiplicacion, type(multiplicacion))
+print("Division: ", division, type(division))
+print("Potencia: ", potencia, type(potencia))
+print("Modulo: ", modulo, type(modulo))
+
+"""
+Jerarquia de operaciones
+2+3*4 -> 14
+(2+3)*4 -> 20
+"""
+
+"""
+Floats
+Python llama floats a cualquier numero con punto decimal.
+Los podemos sumas (+), restar (-), multiplicar (*), divididir (/).
+Les podemos aplicar potencias(**2, **3, **3)
+"""
+
+print(0.1+0.1)
+print(0.2-0.2)
+print(0.1*2)
+print(2*0.2)
+
+# Imprimir la edad de alguien
+
+age = 33 
+message2 = "Charly tiene " + str(age) + " años."
+print(message2)
+
+"""
+TypeError: Esto significa que python no puede reconicer el tipo de 
+informacion que se esta utilizando.
+"""
