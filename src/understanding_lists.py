856871b3ceb50bec2ef94afcf14103bafa194a79
Tu Nombre <tu@email.com>
2025-11-05 10:32:23 -0600

    Aprendimos a remover y agregar cosas

--- src/understanding_lists.py
+++ src/understanding_lists.py
@@ -43,3 +43,68 @@ print("Metodos de las listas para agregar elementos: . append(element)")
 bicycles.append("ducatti")
 print(bicycles)
 
+"""
+Agregar elementos a una lista.
+- append(): Agrega un elemento al final de la lista.
+
+"""
+print("\n--- Agregar elementos a una lista metodo append()---")
+motorcycles = ['honda', 'yamaha', 'susuky']
+print(motorcycles) #Salida: ['honda', 'yamaha', 'susuky']
+motorcycles.append('ducati')
+print(motorcycles) #Salida: ['honda', 'yamaha', 'susuky', 'ducati']
+"""
+Agregar elementos en una posicion especifica
+-insert(): Inserta un elemento en una posicion especifica.
+""" 
+print("\n--- Agregar elementos a una lista metodo insert()---")
+motorcycles = ['honda', 'yamaha', 'susuky']
+print(motorcycles) #Salida: ['honda', 'yamaha', 'susuky']
+motorcycles.insert(1,'ducati')
+print(motorcycles) #Salida: ['honda', 'ducati', 'susuky', 'yamaha']
+
+"""
+Eliminar elementos de una lista y usar el valor eliminado
+- pop(): Elimina y devuelve el ultimo elemento de la lista.
+"""
+print("\n--- Eliminar elementos de una lista .pop()---")
+motorcycles = ['honda', 'yamaha', 'susuky']
+print(motorcycles) #Salida: ['honda', 'yamaha', 'susuky']
+last_motocycles = motorcycles.pop()
+print(motorcycles) #Salida: ['honda','yamaha']
+
+"""
+Eliminar un elemento especifico de una lista por valor
+- pop(index): Elimina y devuelve el elemento en la posicion
+especificada.
+"""
+print("\n--- Eliminar un elemento especifico .pop(index)---")
+motorcycles = ['honda', 'yamaha', 'susuky']
+print(motorcycles) #Salida: ['honda', 'yamaha', 'susuky']
+last_motocycles = motorcycles.pop(1)
+print(motorcycles) #Salida: ['honda','susuky']
+
+"""
+Elimina un elemento especifico de una lista por valor
+- remove(): Elimina la primera aparicion de un valor especifico en
+la lista
+"""
+print("\n--- Eliminar un elemento especifico de una lista por valor metodo remove---")
+motorcycles = ['honda', 'yamaha', 'susuky', 'ducati']
+print(motorcycles) #Salida: ['honda', 'yamaha', 'susuky', 'ducati']
+motorcycles.remove('ducati')
+print(motorcycles) #Salida: ['honda','yamaha','susuky']
+print("\n")
+
+"""
+Organizar una lista permanentemente
+-sport(): Ordena la lista en orden alfabetico.
+"""
+print("\n--- Organizar una lista permanentemente metodo sport---")
+motorcycles = ['honda', 'yamaha', 'susuky', 'ducati']
+print(motorcycles) #Salida: ['honda', 'yamaha', 'susuky', 'ducati']
+motorcycles.sort()
+print(motorcycles) #Salida: ['ducati', 'honda', 'susuky', 'yamaha']
+print("\n")
+motorcycles.sort(reverse=True)
+print(motorcycles)
