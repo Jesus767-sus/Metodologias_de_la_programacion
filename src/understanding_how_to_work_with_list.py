0d4b422e0ab80a100e950eb8766bf39dc67524ef
Tu Nombre <tu@email.com>
2025-11-10 10:40:55 -0600

    Aprendimos metodos built-in

--- src/understanding_how_to_work_with_list.py
+++ src/understanding_how_to_work_with_list.py
@@ -17,7 +17,7 @@ for magican in magicians:
 print("\nGracias a todos, fue un gran espectaculo.")
 """
 Identacion:
-Python utiliza la identacion para determonar cuando una linea
+Python utiliza la identacion para determinar cuando una linea
 de codigo esta conectada a la linea de codigo anterior.
 
 Basicamente, se utilizan 4 espacios en blanco para obligarnos
@@ -34,3 +34,60 @@ print(f"I can't wait to see your next trick, {magician.title()}")
 # Identacion inncecesaria
 message = "Hola Python"
 print(message)
+
+"""
+Las listas tambien pueden almacenar numeros y de hecho,
+son ideales para eso.
+Python ofrece una gran cantidad de herramientas que ayudan
+a trabajar eficientemente con listas de numeros.
+"""
+
+# Metodo built-in rage()
+"""
+El metodo range() nos ayuda a crear facilmente series de numeros:
+Ejemplo:
+"""
+print("Numeros del 0 al 9")
+for value in range(10): # 10 numeros entre 0-9
+    print(value)
+print("Numeros del 1 al 9")
+for value in range(1, 10): # 10 numeros entre 1-9
+    print(value)
+
+print("Numeros impares del 1 al 9")
+for value in range(1,10,2): # 10 numeros entre 1 y 9
+    print(value)
+odd_numbers = list(range(1,10,2))
+print(odd_numbers)
+print("Numeros pares del 1 al 10")
+for value in range(0,11,2):
+    print(value)
+even_numbers = list(range(0,11,2))
+print(even_numbers)
+
+print("Tabla del 9")
+for value in range(0,91,9):
+    print(value)
+Tab_9 = list(range(0,91,9))
+print(Tab_9)
+
+# Cuadrados de los primeros 10 numero
+Squares = []
+for number in range(1, 11):
+    square = number**2
+    Squares.append(square)
+print(Squares)
+
+## Mas metodos built-in
+# Metodo min()
+print("Metodos built-in")
+digits = [1, 2 , 3, 4, 5, 6, 7, 8, 9, 0]
+print(min(digits))
+
+# Metodo max()
+digits = [1, 2 , 3, 4, 5, 6, 7, 8, 9, 0]
+print(max(digits))
+
+# Metodo sum()
+digits = [1, 2 , 3, 4, 5, 6, 7, 8, 9, 0]
+print(sum(digits))
