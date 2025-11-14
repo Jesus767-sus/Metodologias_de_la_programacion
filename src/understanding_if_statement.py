1d889db3914e2228b0edc1f1b918b266bb5e1498
Tu Nombre <tu@email.com>
2025-11-14 07:52:57 -0600

    Vimos multiplos condicionales y variables de tipo BOOLEANO

--- src/understanding_if_statement.py
+++ src/understanding_if_statement.py
@@ -40,3 +40,52 @@ print(age > 21)  # False
 print(age >= 21) # False
 
 #.Multiple conditions  
+age_0 = 22
+age_1 = 18
+
+print("Multiples condiciones")
+print("Operacion or - pseint (O)")
+print( age_0 >= 21 or age_1 >= 21)
+print( age_0 >= 21 or age_1 >= 18)
+
+print("Multiples condiciones")
+print("Operacion and - pseint (Y)")
+print( age_0 >= 21 and age_1 >= 21)
+print( age_0 >= 21 and age_1 >= 18)
+
+print("\n A value is in a list")
+# ¿Como nos preguntamos si algun valor esta en una lista?
+requested_toppings = ['mushrooms', 'onions', 'pineapple']
+print('mushrooms' in requested_toppings) # True
+print('pepperoni' in requested_toppings) # False
+
+# A value not in a list
+banned_users = ['gabriel', 'max', 'andrik', 'quevedo', 'chris', 'angel']
+user = 'pedro'
+print(user not in banned_users) # True
+
+# Variables de tipo BOOLEANOS
+game_active = True
+can_edit = False
+
+"""
+if statement
+
+if condition:
+    do something
+
+if condition:
+    do something (True)
+else:
+    do something (False)
+"""
+
+# Preguntar la edad del usuario y decirle si tiene la edad suficiente para votar.
+# input("") -> str
+age = input("Escribe tu edad: ")
+print(f"\n Tu edad es: {age}")
+
+if int(age)>=18:
+    print("Tienes la edad suficiente para votar.")
+else:
+    print("No tienes la edad suficiente para votar.")
